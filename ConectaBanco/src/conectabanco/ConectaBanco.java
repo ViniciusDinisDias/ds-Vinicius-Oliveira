bd0efa1ece40e8fedcb09e4326ccf088a6184e5a
ViniciusDinisDias <164687372+ViniciusDinisDias@users.noreply.github.com>
2025-08-24 21:12:10 -0300

    Update ConectaBanco.java

--- ConectaBanco/src/conectabanco/ConectaBanco.java
+++ ConectaBanco/src/conectabanco/ConectaBanco.java
@@ -15,45 +15,43 @@ public class ConectaBanco {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-      Scanner sc = new Scanner(System.in);
-      int opcao;
-      
-      do {
-          System.out.println("\n====Menu====");
-          System.out.println("1 - Inserir Aluno");
-          System.out.println("1 - Consultar Alunos");
-          System.out.println("0 - Sair");
-          System.out.println("Escolha uma opção:");
-          opcao = sc.nextInt();
-          sc.nextLine();
-          
-          switch (opcao){
-              case 1:
-                  System.out.println("Digite o código do aluno");
-                  
-                  int codigo = sc.nextInt();
-                  System.out.println("Digite o nome do aluno: ");
-                  String nome = sc.nextLine();
-                  Inserir_Dados.InserirAluno(codigo_aluno, nome_aluno);
-                  break;
-                  
-              case 2:
-                  Consultar.Consultardados();
-                  break;
-                  
-              case 0:
-                  System.out.println("Parando");
-                  
-                  default: 
-                      System.out.println("Opção inválida");
-          
-            } 
-        } while (opcao != 0);
-                
-      sc.close();
-    }
-} 
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int opcao;
 
+    do {
+        System.out.println("\n====Menu====");
+        System.out.println("1 - Inserir Aluno");
+        System.out.println("2 - Consultar Alunos");
+        System.out.println("0 - Sair");
+        System.out.println("Escolha uma opção:");
+        opcao = sc.nextInt();
+        sc.nextLine();
+
+        switch (opcao) {
+            case 1:
+                System.out.println("Digite o código do aluno: ");
+                int codigo = sc.nextInt();
+                sc.nextLine(); // limpar buffer
+                System.out.println("Digite o nome do aluno: ");
+                String nome = sc.nextLine();
+
+                Inserir_Dados.InserirAluno(String.valueOf(codigo), nome);
+                break;
+
+            case 2:
+                Consulta.Consultar_Dados();
+                break;
+
+            case 0:
+                System.out.println("Parando");
+                break;
+
+            default:
+                System.out.println("Opção inválida");
+        }
+    } while (opcao != 0);
+
+    sc.close();
+}
 
